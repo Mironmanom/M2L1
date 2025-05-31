a24757a85fd59136353fd4410c4338fbcc6f9353
Mironmanom <mironlearn@gmail.com>
2025-05-31 17:09:11 +0500

    Add files via upload

---
+++ M2L1/main.py
@@ -0,0 +1,31 @@
+import telebot
+import os
+import random
+bot = telebot.TeleBot("7704115421:AAHykbkd5vqZPMuRj5w-9WJX26g0UZIhGJw")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    mimages = os.listdir('imagesmem')
+    mimage = random.choice(mimages)
+    with open(f'images/{mimage}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)     
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+ 
+    
+bot.polling()
